c135fdedf9926f162f6eb468c7fab9d91c02455b
johnmalek312 <77510343+johnmalek312@users.noreply.github.com>
2025-09-13 17:15:35 +1000

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.droidrun.portal"
         minSdk = 30
         targetSdk = 34
-        versionCode = 35
-        versionName = "0.3.6"
+        versionCode = 37
+        versionName = "0.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
